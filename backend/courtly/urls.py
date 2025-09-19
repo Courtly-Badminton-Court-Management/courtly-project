fdd1640e7be657c2c5a30c97444104473be061a0
proud <proud@email.com>
2025-09-19 19:54:00 +0700

    clearr all pycache from git remote repo it should be ignore

--- backend/courtly/urls.py
+++ backend/courtly/urls.py
@@ -22,9 +22,6 @@ urlpatterns = [
     path("api/auth/token/refresh/", TokenRefreshView.as_view(), name="token-refresh"),
     path("api/auth/me/",       MeView.as_view(),       name="me"),
 
-    # YOUR PATTERNS
-    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
-    # Optional UI:
     path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
     path("api/auth/", include("accounts.urls")),
