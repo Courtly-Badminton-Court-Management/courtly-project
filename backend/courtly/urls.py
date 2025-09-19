82f4e4d58938d2b86b7c7caec303d914a50c9216
root <root@Hokori.localdomain>
2025-09-19 20:22:39 +0700

    add api schema url back

--- backend/courtly/urls.py
+++ backend/courtly/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
     path("api/auth/token/refresh/", TokenRefreshView.as_view(), name="token-refresh"),
     path("api/auth/me/",       MeView.as_view(),       name="me"),
 
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
     path("api/auth/", include("accounts.urls")),
