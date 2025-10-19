2114705be18b560e71e7cdc8a0857439682ae96a
ProudHokori <proundkrafpom@gmail.com>
2025-10-19 23:45:57 +0700

    chore(styling): adjust styling layout and color theme

--- frontend/src/app/(player)/layout.tsx
+++ frontend/src/app/(player)/layout.tsx
@@ -6,7 +6,7 @@ export default function PlayerLayout({ children }: { children: ReactNode }) {
   return (
     <>
       <PlayerNavBar />
-      <main className="mx-auto max-w-6xl px-4 py-6">{children}</main>
+      <main className="mx-auto max-w-7xl px-4 py-4">{children}</main>
     </>
   );
 }
